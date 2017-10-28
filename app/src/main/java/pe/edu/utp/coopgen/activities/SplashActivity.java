abea65599f89dcbe73d1e30f0c541f9045443665
Rafael A. Moreno <1523559@utp.edu.pe>
2017-10-28 03:18:14 -0500

    SplashActivity
    Configurando Splash

--- app/src/main/java/pe/edu/utp/coopgen/activities/SplashActivity.java
+++ app/src/main/java/pe/edu/utp/coopgen/activities/SplashActivity.java
@@ -1,6 +1,8 @@
-package pe.edu.utp.coopgen;
+package pe.edu.utp.coopgen.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +11,23 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import pe.edu.utp.coopgen.R;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        },6000);
+
     }
 
     @Override
